48c7b1abe83e1d64ba069e68fb6372d59d73842e
4MN35T <113859255+4MN35T@users.noreply.github.com>
2023-03-21 18:33:15 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import "../styles/App.css";
 import Child from "./Child";
-
 const App = () => {
-    const [age, setAge] = useState(0);
-  
+    const [age, setAge] = useState(19);
     function incrementAge() {
-      setAge(age + 1);
+        setAge(age + 1);
     }
-  
-    return <Child age={age} incrementAge={incrementAge} />;
-  };
-  
-  export default App;
+
+    return (
+    <div>
+      <Child age={age} incrementAge={incrementAge} />
+    </div>
+  );
+};
+
+export default App;
